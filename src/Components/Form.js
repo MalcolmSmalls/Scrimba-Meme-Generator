a0d169c2e1f56a8214918da55f2049e61e23bb35
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-12 11:15:38 -0800

    Change form to div, add event listener to button to console log meme url each click

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react"
+import memesData from "../memesData"
 
 function Form(){
+    function getData () {
+        console.log(memesData.data.memes[Math.floor(Math.random() * 99)].url)
+    }
     return (
         <div className = "form-container">
-            <form>
+            <div className = "form">
                 <input className = "first-input" placeholder = "Shut up"></input>
                 <input className = "second-input" placeholder = "and take my money"></input>
-                <button className = "submit">Get a new image 🖼️</button>
-            </form>
+                <button className = "submit" onClick = {getData}>Get a new image 🖼️</button>
+            </div>
         </div>
     )
 }
